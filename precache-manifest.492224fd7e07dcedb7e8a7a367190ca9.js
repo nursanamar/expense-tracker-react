2a2f664395491c204bd120c2c5521521cbcb9686
Nursan amar <nursan011@gmail.com>
2021-03-15 21:44:06 +0800

    initiaal commit

---
+++ precache-manifest.492224fd7e07dcedb7e8a7a367190ca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ebdc310b0f11d5e5d7cf65f65f97f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "645433df75991264c9d5",
+    "url": "/static/css/main.ea157d10.chunk.css"
+  },
+  {
+    "revision": "1d451f595efbb43016ab",
+    "url": "/static/js/2.6ebd275a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6ebd275a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "645433df75991264c9d5",
+    "url": "/static/js/main.8aaba670.chunk.js"
+  },
+  {
+    "revision": "3c7f62721c30427bb531",
+    "url": "/static/js/runtime-main.7ce61b84.js"
+  }
+]);
